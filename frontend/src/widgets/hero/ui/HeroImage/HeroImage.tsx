7b39467eee728b10d84afb5c538ddcb50f143b8a
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-10-01 23:04:01 +0500

    Сделан блок hero

--- frontend/src/widgets/hero/ui/HeroImage/HeroImage.tsx
+++ frontend/src/widgets/hero/ui/HeroImage/HeroImage.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import ImageMain from "../../assets/images/main.png";
+import styles from './HeroImage.module.scss'
 
 export const HeroImage = () => {
   return (
-    <div>
-      <div>
-        <Image src={ImageMain} alt="image main" width={547} height={535} />
-      </div>
+    <div className={styles.container}>
+      <Image src={ImageMain} alt="image main" width={547} height={535} />
     </div>
   );
 }
