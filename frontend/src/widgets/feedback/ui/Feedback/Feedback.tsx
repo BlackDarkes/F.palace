980e67c002c3cdf6a8a91aec536a8d7a5b5de086
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-10-05 04:09:26 +0500

    Сделан блок feedback

--- frontend/src/widgets/feedback/ui/Feedback/Feedback.tsx
+++ frontend/src/widgets/feedback/ui/Feedback/Feedback.tsx
@@ -6,6 +6,7 @@ import { FEEDBACK } from "../../models/feedback";
 import { useState } from "react";
 import { FeedbackButtons } from "../FeedbackButtons/FeedbackButtons";
 import { FeedbackImage } from "../FeedbackImage/FeedbackImage";
+import styles from './Feedback.module.scss'
 
 export const Feedback = () => {
   const { isLoading, error, data: feedback} = useFeedbackAll();
@@ -36,11 +37,11 @@ export const Feedback = () => {
   }
 
   return (
-    <section>
-      <Container>
-        <div>
+    <section className={styles.feedback}>
+      <Container className={styles.feedbackContainer}>
+        <div className={styles.feedbackInfo}>
           <Info info={{ ...FEEDBACK, body: feedback[count].message}} />
-          <FeedbackButtons feedback={feedback[count]} nextFeedback={nextFeedback} previewFeedback={previewFeedback} />
+          <FeedbackButtons count={count} feedback={feedback[count]} feedbackLength={feedback.length} nextFeedback={nextFeedback} previewFeedback={previewFeedback} />
         </div>
 
         <FeedbackImage />
--- frontend/src/widgets/feedback/ui/FeedbackButtons/FeedbackButtons.tsx
+++ frontend/src/widgets/feedback/ui/FeedbackButtons/FeedbackButtons.tsx
@@ -1,24 +1,46 @@
 import { IFeedback } from "@/features/feedback";
 import IconArrow from "../../assets/icons/arrow.svg";
+import styles from "./FeedbackButtons.module.scss";
 
 interface IFeedbackButtonsProps {
+  count: number;
   feedback: IFeedback;
+  feedbackLength: number;
   nextFeedback: () => void;
   previewFeedback: () => void;
 }
-  
-export const FeedbackButtons = ({ feedback, nextFeedback, previewFeedback }: IFeedbackButtonsProps) => {
+
+export const FeedbackButtons = ({
+  count,
+  feedback,
+  feedbackLength,
+  nextFeedback,
+  previewFeedback,
+}: IFeedbackButtonsProps) => {
   return (
-    <div>
+    <div className={styles.block}>
       <div>
-        <h3>{feedback.name}</h3>
-        <p>Product Designer @ Kitty</p>
+        <h3 className={styles.blockName}>{feedback.name}</h3>
+        <p className={styles.blockTag}>Product Designer @ Kitty</p>
       </div>
-      
-      <div>
-        <button type="button" onClick={previewFeedback}><IconArrow /></button>
-        <button type="button" onClick={nextFeedback}><IconArrow /></button>
+
+      <div className={styles.blockArrows}>
+        <button
+          type="button"
+          onClick={previewFeedback}
+          className={`${styles.blockArrow} ${styles.blockArrowFirst} ${count === 0 ? styles.blockArrowDisable : ""}`}
+          disabled={count === 0 ? true : false}
+        >
+          <IconArrow />
+        </button>
+        <button
+          type="button"
+          onClick={nextFeedback}
+          className={`${styles.blockArrow} ${count === feedbackLength - 1 ? styles.blockArrowDisable : ""}`}
+        >
+          <IconArrow />
+        </button>
       </div>
     </div>
   );
-}
+};
